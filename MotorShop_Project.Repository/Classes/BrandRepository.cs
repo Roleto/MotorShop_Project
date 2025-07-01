2fe55514ad343956613668aea2d6afe30d99f37c
Roland Gulya <mgulyaroland@gmail.com>
2025-07-01 18:48:19 +0200

    BrandRepo Created

---
+++ MotorShop_Project.Repository/Classes/BrandRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotorShop_Project.Data.DBContext;
+using MotorShop_Project.Data.Entities;
+using MotorShop_Project.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorShop_Project.Repository.Classes
+{
+    public class BrandRepository : IRepository<BrandEntity>
+    {
+        private MotorShopDbContext context;
+
+        public BrandRepository(MotorShopDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(BrandEntity item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            context.Brands.Add(item);
+            context.SaveChanges();
+        }
+        public BrandEntity Read(int id)
+        {
+            var readItem = ReadAll().FirstOrDefault(x => x.Id == id);
+
+            if (readItem == null)
+                throw new KeyNotFoundException($"No Brand found with Id {id}");
+
+            return readItem;
+        }
+        public void Update(BrandEntity item)
+        {
+                var oldItem = Read(item.Id);
+
+                oldItem.Name = item.Name;
+                oldItem.Models = item.Models;
+                oldItem.Alt = item.Alt;
+                oldItem.ImgUrl = item.ImgUrl;
+
+                context.SaveChanges();
+        }
+
+        public void Delete(BrandEntity item)
+        {
+            context.Brands.Remove(item);
+            context.SaveChanges();
+        }
+        
+        public IEnumerable<BrandEntity> ReadAll()
+        {
+            return context.Set<BrandEntity>();
+        }
+
+    }
+}
---
+++ MotorShop_Project.Repository/Interface/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorShop_Project.Repository.Interface
+{
+    public interface IRepository<T>
+    {
+        void Create(T item);
+        T Read(int id);
+        void Update(T item);
+        void Delete(T item);
+        IEnumerable<T> ReadAll();
+    }
+}
